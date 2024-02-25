24fa77733039d589771dcdca83ff981e6a8d4553
Elena S <prostovael@yandex.ru>
2024-02-25 21:06:11 +0400

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int amount = 20_000; // стоимость билета
         int mile = 20; // количество рублей для одной бонусной мили
-        int bonus = amount/mile;
+        int bonus = amount / mile;
 
         System.out.println(bonus); // количество бонусных миль
 
